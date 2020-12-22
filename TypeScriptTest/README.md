6c83326f7dc52c5bbe8736a7b2419861c74f4020
Atria <atriasft@gmail.com>
2020-12-22 22:48:52 +0900

    add TypeScriptTest

---
+++ TypeScriptTest/README.md
@@ -0,0 +1,3 @@
+# TypeScriptTest
+これは [BlazorでTypeScriptを利用する - Qiita](#) のサンプルです。   
+TypeScriptを利用したBlazorでの開発を提案するサンプルコードです。
---
+++ TypeScriptTest/TypeScriptTest/wwwroot/TypeScript.ts
@@ -0,0 +1,17 @@
+﻿namespace Test {
+    class Functions {
+        //与えられた文字列をalertで表示します
+        public showAlert(message: string){
+            alert(message);
+        }
+        //与えられた文字列を編集して返します。
+        public editText(message: string): string {
+            return message+" : edited";
+        }
+    }
+    export function Load(): void {
+        window['Functions'] = new Functions();
+    }
+}
+
+Test.Load();
